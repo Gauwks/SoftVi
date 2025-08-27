1d2b5b5cbf4c2952973a00f277668378102e920e
Gauwks <maknae.games7@gmail.com>
2025-08-26 21:44:55 -0300

    Atualizando

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header(){
+    return (
+       <div className="Header">
+           <div className="navbar">
+             <a href=""> Atendimento</a>
+             <a href=""> Entre ou Cadastre-se</a>
+             <a href=""> Carrinho</a>
+           </div>
+       </div>
+    );
+}
+
+export default Header
